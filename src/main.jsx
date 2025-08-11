c2d4e277afed7cc0c6553afc8351f90b4a12063e
bokyeong58 <koobg0813@naver.com>
2025-08-11 17:51:51 +0900

    main

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CafeSimulator from './CafeSimulator'; // 아까 만든 CafeSimulator.jsx 경로
+import './styles.css'; // 스타일 파일 불러오기
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CafeSimulator />
+  </React.StrictMode>
+);
